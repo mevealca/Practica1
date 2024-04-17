b43272e177021d38759d39a888a7100fd7014733
Miguel Eve <mevealcantara@gmail.com>
2024-04-16 23:28:21 -0400

    Primera practica Abstraccion y Polimorfismo

---
+++ APPractice.Domain/APPractice.Domain/practica1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace APPractice.Domain
+{
+    public abstract class Person
+    {
+        public string? Name { get; set; }
+        public int? Age { get; set; }
+
+        public Person(string name, int age) {
+
+            Name = name;
+            Age = age;
+
+        }
+
+        public abstract void Imprimir();
+    }
+
+    public class Employee : Person
+    {
+        public Employee(string name,int age,double salary): base(name,age){
+
+            Salary = salary;
+        
+        }
+        public double Salary { get; set; }
+        public override void Imprimir()
+        {
+            Console.WriteLine("Name: {0} Salary: {1}",Name,Salary);
+
+        }
+    }
+
+    public class Program {
+
+        public static void Main() { 
+        
+            Person person = new Employee("Juan Ernesto",25,15000);
+            person.Imprimir();
+
+            Employee employee = new Employee("Francisco Javier", 30, 50000);
+            employee.Imprimir();
+        }
+    
+    }
+}
